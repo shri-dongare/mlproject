c369e412e8afbbb01272d88bf128a00dd16ce897
shri-dongare <dongares666@gmail.com>
2026-07-11 19:16:18 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 import logging
 
 def error_message_detail(error,error_detail:sys):
@@ -17,4 +18,6 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
     
